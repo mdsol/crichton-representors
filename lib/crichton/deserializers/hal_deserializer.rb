49987d369e049f33b23d58bef48dadf7c0692fa4
Jordi Carres <jcarres@mdsol.com>
2014-05-08 22:44:40 +0900

    hal deserializer

--- lib/crichton/deserializers/hal_deserializer.rb
+++ lib/crichton/deserializers/hal_deserializer.rb
@@ -4,13 +4,19 @@ module Crichton
 
   class HalDeserializer
 
-    def initialize(document)
-      @json = JSON.parse(document)
+    def initialize(document_or_hash)
+      if document_or_hash.is_a?(Hash)
+        @json = document_or_hash
+      else
+        @json = JSON.parse(document_or_hash)
+      end
     end
 
     def deserialize
       representor = Golem.new
       deserialize_properties(representor, @json)
+      deserialize_links(representor, @json)
+      deserialize_embedded(representor, @json)
       representor
     end
 
@@ -19,12 +25,40 @@ module Crichton
     end
 
     private
+
     def deserialize_properties(representor, json)
       property_names = json.keys.select{|key| !key.start_with?('_') }
       property_names.each do |property_name|
         representor.create_property(property_name, json[property_name])
       end
     end
-  end
 
+    def deserialize_links(representor, json)
+      links = json["_links"] || {}
+      link_multiple = links.values.select {|value| value.is_a?(Array)}
+      links.each do |link_name, link_values|
+        if link_values.is_a?(Array)
+          representor.create_link_array(link_name, link_values)
+        else
+          representor.create_link(link_name, link_values)
+        end
+      end
+    end
+
+    def deserialize_embedded(representor, json)
+       embedded = json["_embedded"] || {}
+       embedded.each do |name, value|
+         if value.is_a?(Array)
+           resources = value.map do |one_embedded_resource|
+             HalDeserializer.new(one_embedded_resource).deserialize
+           end
+           representor.create_embedded(name, resources)
+         else
+           new_representor = HalDeserializer.new(value).deserialize
+           representor.create_embedded(name, new_representor)
+         end
+       end
+    end
+
+  end
 end
--- lib/crichton/golem.rb
+++ lib/crichton/golem.rb
@@ -1,15 +1,45 @@
 module Crichton
+
+  class PropertiesBag < Hash
+    def initialize(hash={})
+      self.merge!(hash)
+    end
+
+    def method_missing(method, *args, &block)
+      if self.has_key?(method.to_s)
+        self[method.to_s]
+      else
+        super
+      end
+    end
+  end
+
   ##
   # Manages the respresentation of hypermedia messages for different media-types.
-  class Golem
+  class Golem < PropertiesBag
 
-    attr_reader :properties
+    attr_reader :properties, :links, :embedded_resources
+
     def initialize
       @properties = {}
+      @links = PropertiesBag.new
+      @embedded_resources = PropertiesBag.new
     end
 
     def create_property(name, value)
       @properties[name] = value
+    end
+
+    def create_link(link_name, values)
+      @links[link_name] = PropertiesBag.new(values)
+    end
+
+    def create_link_array(link_name, array_of_links)
+      @links[link_name] = array_of_links.map {|link_properties| PropertiesBag.new(link_properties)}
+    end
+
+    def create_embedded(name, embedded_resource)
+      @embedded_resources[name] = embedded_resource
     end
 
     def method_missing(method, *args, &block)
@@ -22,4 +52,3 @@ module Crichton
   end
 
 end
-
