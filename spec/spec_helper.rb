3dee6c5d7205844107e5bf67824e25b33f4ccc6b
Jordi Carres <mumismo@gmail.com>
2014-05-18 18:59:40 +0900

    Support mutant and update gem to proper names

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,18 +9,18 @@ require 'debugger'
 require 'bundler'
 require 'simplecov'
 
-SimpleCov.start
 Debugger.start
 Bundler.setup
 
-require 'crichton_representors'
+require 'crichton-representors'
 
 Dir["#{SPEC_DIR}/support/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
