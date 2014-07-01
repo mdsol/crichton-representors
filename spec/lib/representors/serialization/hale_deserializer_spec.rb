b8d167846e9c3290f0d8059471ddf5eeb42c6354
Jordi Polo <mumismo@gmail.com>
2014-07-01 13:27:38 +0200

    one spec for meta

--- spec/lib/representors/serialization/hale_deserializer_spec.rb
+++ spec/lib/representors/serialization/hale_deserializer_spec.rb
@@ -18,6 +18,39 @@ module Representors
       it_behaves_like 'can create a representor from a hal document' do
         subject(:deserializer) {Representors::HaleDeserializer.new(document)}
       end
+
+      context '_meta with properties and links' do
+        subject(:deserializer) {Representors::HaleDeserializer.new(document)}
+        let(:semantics) { { 'title' => 'The Neverending Story'}}
+        let(:transition_rel) { 'author'}
+        let(:transition_href) { '/mike'}
+        let(:document) do
+          {
+            'title' => 'The Neverending Story',
+            '_links' => {
+              'author' => {'href' => '/mike'}
+            },
+            '_meta' => {
+              'any' => { 'json' => 'thing'}
+            }
+          }
+
+          it 'return a hash with all the attributes of the document' do
+            expect(semantics_field).to eq(semantics)
+          end
+          it 'Create a transition with the link' do
+            expect(transitions_field.size).to eq(1)
+            expect(transitions_field.first.rel).to eq(transition_rel)
+            expect(transitions_field.first.uri).to eq(transition_href)
+          end
+          it 'does not return any embedded resource' do
+            expect(embedded_field).to be_empty
+          end
+
+        end
+
+      end
+
     end
 
   end
