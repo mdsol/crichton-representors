2cf5fc9780c211428623af787b2360a741fe2beb
Jordi Polo <mumismo@gmail.com>
2014-07-01 09:57:40 +0200

    moved hal document specs to a different file so it can be used by hale also

--- lib/representors/serialization/hale_deserializer.rb
+++ lib/representors/serialization/hale_deserializer.rb
@@ -4,5 +4,21 @@ module Representors
   class HaleDeserializer < HalDeserializer
     media_symbol :hale
     media_type 'application/vnd.hale+json'
+
+    META_KEY = '_meta'.freeze
+    RESERVED_KEYS = [LINKS_KEY, META_KEY, EMBEDDED_KEY]
+
+
+    private
+
+    # Very complex but this will be called by the setup_serialization(media) in HalDeserializer
+    # TODO: refactor
+    def builder_add_from_deserialized!(builder, data_as_a_hash)
+      deserialize_properties!(builder, data_as_a_hash)
+
+      deserialize_links!(builder, data_as_a_hash)
+      deserialize_embedded!(builder, data_as_a_hash)
+    end
+
   end
 end
--- spec/lib/representors/serialization/hale_deserializer_spec.rb
+++ spec/lib/representors/serialization/hale_deserializer_spec.rb
@@ -5,13 +5,20 @@ module Representors
     it 'inherits from HalDeserializer' do
       expect(HaleDeserializer.class.ancestors.include? HalDeserializer)
     end
-  
+
     it 'provides the media type application/vnd.hale+json' do
       expect(HaleDeserializer.media_types).to include('application/vnd.hale+json')
     end
-  
+
     it 'provides the media symbol :hale' do
       expect(HaleDeserializer.media_symbols).to include(:hale)
     end
+
+    describe "#to_representor" do
+      it_behaves_like 'can create a representor from a hal document' do
+        subject(:deserializer) {Representors::HaleDeserializer.new(document)}
+      end
+    end
+
   end
 end
