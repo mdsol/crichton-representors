4a26ae87e6f253d5d0f98144bdaebcde517449d7
Connor Savage <csavage@mdsol.com>
2014-10-27 15:16:23 -0400

    refactor hale deserializer to consume new builder

--- lib/representors/serialization/hale_deserializer.rb
+++ lib/representors/serialization/hale_deserializer.rb
@@ -6,18 +6,29 @@ module Representors
     media_type 'application/vnd.hale+json'
 
     META_KEY = '_meta'.freeze
-    RESERVED_KEYS = HalDeserializer::RESERVED_KEYS +  [META_KEY]
+    REF_KEY = '_ref'.freeze
+    RESERVED_KEYS = HalDeserializer::RESERVED_KEYS +  [META_KEY, REF_KEY]
 
     private
 
-    # Very complex but this will be called by the setup_serialization(media) in HalDeserializer
-    # TODO: refactor
-    def builder_add_from_deserialized!(builder, data_as_a_hash)
-      deserialize_properties!(builder, data_as_a_hash)
-
-      deserialize_links!(builder, data_as_a_hash)
-      deserialize_embedded!(builder, data_as_a_hash)
+    def builder_add_from_deserialized(builder, media)
+      media = dereference_meta_media(media)
+      super    
     end
 
+    def dereference_meta_media(media)
+      media = media.dup
+      meta_info = media.delete(META_KEY) # Remove _meta from media to prevent serialization as property
+      nested_find_and_replace!(media, meta_info, REF_KEY)
+      media
+    end
+
+    def nested_find_and_replace!(obj, metas, target_key)
+      if obj.respond_to?(:key?) && obj.key?(target_key)
+        obj.delete(target_key).each { |ref| obj[ref] = metas[ref] }
+      elsif [Array, Hash].include?(obj.class)
+        obj.each { |*el| nested_find_and_replace!(el.last, metas, target_key) }
+      end
+    end
   end
 end
