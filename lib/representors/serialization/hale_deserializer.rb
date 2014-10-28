b56d718f7b0f077d9f5ddc06ae75a9170bfe128a
Connor Savage <csavage@mdsol.com>
2014-10-28 18:44:29 -0400

    Refactor media manipulation madness

--- lib/representors/serialization/hale_deserializer.rb
+++ lib/representors/serialization/hale_deserializer.rb
@@ -7,79 +7,91 @@ module Representors
 
     META_KEY = '_meta'.freeze
     REF_KEY = '_ref'.freeze
+    DATA_KEY = 'data'.freeze
     RESERVED_KEYS = HalDeserializer::RESERVED_KEYS +  [META_KEY, REF_KEY]
-
-    private
+    RESERVED_FIELD_VALUES = Field::SIMPLE_METHODS + [Field::NAME_KEY, Field::SCOPE_KEY, Field::OPTIONS_KEY, Field::VALIDATORS_KEY, Field::DESCRIPTORS_KEY]
 
     #TODO Make this not terrible (refactor hal deserializer to inherit from hale deserializer)
     # move all logic here
     def builder_add_from_deserialized(builder, media)
       media = dereference_meta_media(media)
-      media = parse_options(media)
-      media = parse_validators(media)
       super    
     end
 
+    def deserialize_links(builder, media)
+      links = media[LINKS_KEY] || {}
+      links.each do |link_rel, link_values|
+        link_values = [link_values] unless link_values.is_a?(Array)
+        ensure_valid_links!(link_rel, link_values)
+        link_values = parse_validators(link_values)
+        link_values = parse_options(link_values)
+        builder = builder.add_transition_array(link_rel, link_values)
+      end
+
+      builder
+    end
+
     private
+
+    def ensure_valid_links!(link_rel, link_values_array)
+      raise(DeserializationError, 'CURIE support not implemented for HAL') if link_rel.eql?(CURIE_KEY)
+
+      if link_values_array.map { |link| link[HREF] }.any?(&:nil?)
+        raise DeserializationError, 'All links must contain the href attribute'
+      end
+    end
+
+    def deep_find_and_transform!(obj, target_key, &blk)
+      if obj.respond_to?(:key) && obj.key?(target_key)
+        yield obj, target_key
+      elsif [Array, Hash].include?(obj.class)
+        obj.each { |*el| deep_find_and_transform!(el.last, target_key, &blk) }
+      end
+    end
 
     def dereference_meta_media(media)
       media = media.dup
       # Remove _meta from media to prevent serialization as property
       meta_info = media.delete(META_KEY)
-      dereference_meta_media!(media, meta_info)
-      media
-    end
-
-    def dereference_meta_media!(obj, metas)
-      if obj.respond_to?(:key?) && obj.key?(REF_KEY)
-        obj.delete(REF_KEY).each { |ref| obj[ref] = metas[ref] }
-      elsif [Array, Hash].include?(obj.class)
-        obj.each { |*el| dereference_meta_media!(el.last, metas) }
+      deep_find_and_transform!(media, REF_KEY) do |media, ref_key|
+        media.delete(ref_key).each { |ref| media[ref] = meta_info[ref] }
       end
+      media
     end
 
     def parse_options(media)
       media = media.dup
-      parse_options!(media)
-      media
-    end
 
-    def parse_options!(obj)
-      if obj.respond_to?(:key?) && obj.key?('options')
-          if !obj['options'].is_a?(Hash) && obj['options'].first.is_a?(Hash)
-            new_options = obj['options'].reduce({}) do |memo, hash|
-              memo.merge!(hash)
-            end
-            obj['options'] = { 'hash' => new_options }
-          elsif !obj['options'].is_a?(Hash)
-            obj['options'] = { 'list' => obj['options'].dup }
+      deep_find_and_transform!(media, 'options') do |media, opt_key|
+        if !media[opt_key].is_a?(Hash) && media[opt_key].first.is_a?(Hash)
+          new_options = media[opt_key].reduce({}) do |memo, hash|
+            memo.merge!(hash)
           end
-      elsif [Array, Hash].include?(obj.class)
-        obj.each { |*el| parse_options!(el.last) }
+          media[opt_key] = { 'hash' => new_options }
+        elsif !media[opt_key].is_a?(Hash)
+          media['options'] = { 'list' => media[opt_key].dup }
+        end
       end
+
+      media
     end
 
     def parse_validators(media)
       media = media.dup
-      parse_validators!(media)
+
+      deep_find_and_transform!(media, 'data') do |media, data_key|
+        media[data_key].each do |field_key, field_value|
+          arr = []
+          field_value.each do |k,v|
+            arr << {k => media[data_key][field_key].delete(k)} unless RESERVED_FIELD_VALUES.include?(k.to_sym)
+          end
+          media[data_key][field_key][Field::VALIDATORS_KEY] = arr unless arr.empty?
+        end
+      end
+
       media
     end
 
-    def parse_validators!(obj)
-      if obj.respond_to?(:key?) && obj.key?('data')
-        obj['data'].each do |field_key, field_value|
-          arr = []
-          field_value.each do |k,v|
-            unless (Field::SIMPLE_METHODS + [Field::NAME_KEY, Field::SCOPE_KEY, Field::OPTIONS_KEY, Field::VALIDATORS_KEY, Field::DESCRIPTORS_KEY]).include?(k.to_sym)
-              arr << {k => obj['data'][field_key].delete(k)}
-            end
-          end
-          obj['data'][field_key]['validators'] = arr unless arr.empty?
-        end
-      elsif [Array, Hash].include?(obj.class)
-        obj.each { |*el| parse_validators!(el.last) }
-      end
-    end
-
   end
+
 end
