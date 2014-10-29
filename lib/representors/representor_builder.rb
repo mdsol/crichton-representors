054be1c1edea3e7928b94b49e2ac77cdf83072ea
Connor Savage <csavage@mdsol.com>
2014-10-29 17:02:21 -0400

    remove symbolize keys, modify to use deep dup and utilities module

--- lib/representors/representor_builder.rb
+++ lib/representors/representor_builder.rb
@@ -1,4 +1,6 @@
+require 'representor_support/utilities'
 require 'representors/representor_hash'
+
 module Representors
 
   ##
@@ -6,6 +8,8 @@ module Representors
   # In the present implementation it will create a hash of a specific format to
   # Initialize the Representor with, this will create classess with it.
   class RepresentorBuilder
+    include RepresentorSupport::Utilities
+
     HREF_KEY = :href
     DATA_KEY = :data
 
@@ -21,7 +25,7 @@ module Representors
     # Adds an attribute to the Representor. We are creating a hash where the keys are the
     # names of the attributes
     def add_attribute(name, value, options={})
-      new_representor_hash = RepresentorHash.new(@representor_hash.to_h)
+      new_representor_hash = RepresentorHash.new(deep_dup(@representor_hash.to_h))
       new_representor_hash.attributes[name] = options.merge({value: value})
       RepresentorBuilder.new(new_representor_hash)
     end
@@ -29,7 +33,7 @@ module Representors
     # Adds a transition to the Representor, each transition is a hash of values
     # The transition collection is an Array
     def add_transition(rel, href, options={})
-      new_representor_hash = RepresentorHash.new(@representor_hash.to_h)
+      new_representor_hash = RepresentorHash.new(deep_dup(@representor_hash.to_h))
       options = symbolize_keys(options)
       options.delete(:method) if options[:method] == Transition::DEFAULT_METHOD
       link_values = options.merge({href: href, rel: rel})
@@ -52,18 +56,9 @@ module Representors
     end
 
     def add_embedded(name, embedded_resource)
-      new_representor_hash = RepresentorHash.new(@representor_hash.to_h)
+      new_representor_hash = RepresentorHash.new(deep_dup(@representor_hash.to_h))
       new_representor_hash.embedded[name] = embedded_resource
       RepresentorBuilder.new(new_representor_hash)
     end
-
-    private
-
-    #TODO move into utility module, possibly make recursive
-    def symbolize_keys(hash)
-      Hash[hash.map{|(k,v)| [k.to_sym,v]}]
-    end
-
-
   end
 end
