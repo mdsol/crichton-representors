3901fd18df8c5c0956770b73fc5a0cf4d79420eb
Connor Savage <csavage@mdsol.com>
2014-11-02 17:06:06 -0500

    add fixtures from hale spec, refactor roundtrip spec to include them

--- spec/lib/representors/serialization/hale_roundtrip_spec.rb
+++ spec/lib/representors/serialization/hale_roundtrip_spec.rb
@@ -3,21 +3,26 @@ require 'spec_helper'
 module Representors
 
   describe 'Hale round trips' do
-    it 'round trips a simple document' do
-      deserialized_representor = HaleDeserializer.new(File.read("#{SPEC_DIR}/support/basic-hale.json")).to_representor
+    after do
+      deserialized_representor = HaleDeserializer.new(@file).to_representor
       serialized_hale = Serialization::HaleSerializer.new(deserialized_representor).to_media_type
       redeserialized_representor = HaleDeserializer.new(serialized_hale).to_representor
       reserialized_hale = Serialization::HaleSerializer.new(redeserialized_representor).to_media_type
-      expect(serialized_hale).to eq(reserialized_hale)
+      expect(JSON.parse(serialized_hale)).to eq(JSON.parse(reserialized_hale))
+    end
+
+    it 'round trips a simple document' do
+      @file = File.read("#{SPEC_DIR}/support/basic-hale.json")
     end
 
     it 'round trips a complex document' do
-      deserialized_representor = HaleDeserializer.new(File.read("#{SPEC_DIR}/fixtures/complex_hale_document.json")).to_representor
-      serialized_hale = Serialization::HaleSerializer.new(deserialized_representor).to_media_type
-      redeserialized_representor = HaleDeserializer.new(serialized_hale).to_representor
-      reserialized_hale = Serialization::HaleSerializer.new(redeserialized_representor).to_media_type
-      expect(serialized_hale).to eq(reserialized_hale)
+      @file = File.read("#{SPEC_DIR}/fixtures/complex_hale_document.json")
     end
 
+    Dir["#{SPEC_DIR}/fixtures/hale_spec_*.json"].each do |path|
+      it "round trips the hale spec #{path[/hale_spec_(.*?)_example/, 1]} document" do
+        @file = File.read(path)
+      end
+    end
   end
 end
