891864a5ed3163a26d9a5e39c8f44233ebdac659
Shea Valentine <svalentine@mdsol.com>
2014-11-11 11:56:08 -0500

    correct hal deserialization

--- spec/lib/representors/serialization/hal_deserializer_spec.rb
+++ spec/lib/representors/serialization/hal_deserializer_spec.rb
@@ -19,5 +19,16 @@ describe Representors::HalDeserializer do
     it_behaves_like 'can create a representor from a hal document' do
       subject(:deserializer) {Representors::HalDeserializer.new(document)}
     end
+    
+    context "when the Hal deserializer recieves a Hale document" do
+      it "deserializes as a Hal document and not a Hale document" do
+        file = File.read("#{SPEC_DIR}/fixtures/hale_tutorial_examples/meta.json")
+        hal_rep = Representors::HalDeserializer.new(file).to_representor
+        place_order_link_data = hal_rep.transitions.find { |t| t[:rel] == "place_order" }#["data"]
+        expect(hal_rep.properties.keys).to include('_meta')
+        expect(place_order_link_data.interface_method).to eq('GET')
+      end
+    end
   end
+  
 end
